6ac380059078edf562d6ad7c8da95bca95a687ea
Ex-iT <vic.toothpick@gmail.com>
2022-09-01 13:21:32 +0200

    Moved API endpoint to server Middleware folder

--- nuxt.config.js
+++ nuxt.config.js
@@ -44,5 +44,10 @@ export default {
   // Build Configuration: https://go.nuxtjs.dev/config-build
   build: {},
 
-  serverMiddleware: ['~/api/v1/index.ts'],
+  serverMiddleware: [
+    {
+      path: '/server-middleware/v1',
+      handler: '~/server-middleware/v1/index.ts',
+    },
+  ],
 }

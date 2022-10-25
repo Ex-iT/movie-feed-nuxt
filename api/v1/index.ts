733176dbe16b04ba3ce89b49e84d8e4bd1523db0
Ex-iT <vic.toothpick@gmail.com>
2022-10-25 15:31:47 +0200

    Added cache to unsupported API methods

--- api/v1/index.ts
+++ api/v1/index.ts
@@ -14,7 +14,7 @@ app.get('/', (_req, res) => {
   res
     .status(418)
     .setHeader('Cache-Control', CACHING_DEFAULT)
-    .json({ ok: true, error: 'Fight The Future' })
+    .json({ ok: false, error: 'Fight The Future' })
 })
 
 app.get('/programmes', async (req, res) => {
@@ -22,7 +22,10 @@ app.get('/programmes', async (req, res) => {
     const programmes = await getProgrammes()
     res.status(200).setHeader('Cache-Control', CACHING_DEFAULT).json(programmes)
   } else {
-    res.status(405).json({ ok: false, error: 'Method Not Allowed' })
+    res
+      .status(405)
+      .setHeader('Cache-Control', CACHING_DEFAULT)
+      .json({ ok: false, error: 'Method Not Allowed' })
   }
 })
 
@@ -31,7 +34,10 @@ app.get('/status', async (req, res) => {
     const status = await getStatus()
     res.status(200).json(status)
   } else {
-    res.status(405).json({ ok: false, error: 'Method Not Allowed' })
+    res
+      .status(405)
+      .setHeader('Cache-Control', CACHING_DEFAULT)
+      .json({ ok: false, error: 'Method Not Allowed' })
   }
 })
 

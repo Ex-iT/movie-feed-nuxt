c30fc43984528a7278e02aad8380d566610e3870
Ex-iT <vic.toothpick@gmail.com>
2023-05-10 11:58:36 +0200

    Update files to match prettier settings

--- lib/getFirestoreDb.ts
+++ lib/getFirestoreDb.ts
@@ -10,7 +10,7 @@ const getFirestoreDb = () => {
     firebaseApp = initializeApp({
       credential: cert({
         projectId: process.env.FIREBASE_PROJECT_ID,
-        privateKey: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, '\n'),
+        privateKey: process.env.FIREBASE_PRIVATE_KEY,
         clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
       }),
     })
